d686a7787cd573efd55af99b40d71e93b9be6429
DESKTOP-96Q00G1\Sakis <sthanasis1994@gmail.com>
2020-07-05 23:41:30 +0300

    connected pug as view engine, added auth, added styles,added utilities

--- server.js
+++ server.js
@@ -3,20 +3,25 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({
-  path: './config.env',
+  path: './.config.env',
 });
 const app = require('./app');
 
-const DB = `mongodb+srv://sakis:${process.env.DB_PASSWORD}@cluster0-fzepy.mongodb.net/test?retryWrites=true&w=majority`;
+const DB = `mongodb://sakis:${process.env.DB_PASSWORD}@cluster0-shard-00-00-nyjez.gcp.mongodb.net:27017,cluster0-shard-00-01-nyjez.gcp.mongodb.net:27017,cluster0-shard-00-02-nyjez.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`;
+
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
+    dbName: 'test',
   })
   .then(() => {
     console.log('DB connection successful');
+  })
+  .catch((err) => {
+    console.log(err);
   });
 
 const port = 3000 || process.env.PORT;
